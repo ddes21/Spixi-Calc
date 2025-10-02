f86f2849459109e9b27f4dce9f9f9c14ede1bca9
ddes21 <desilvadinil@gmail.com>
2025-10-03 01:45:28 +1000

    Point to latest release

--- app/js/spixi-app-sdk.js
+++ app/js/spixi-app-sdk.js
@@ -15,20 +15,5 @@
 var SpixiAppSdk = {
     version: 0.3,
     date: "2025-07-31",
-    fireOnLoad: function () { location.href = "ixian:onload"; },
     back: function () { location.href = "ixian:back"; },
-    sendNetworkData: function (data) { location.href = "ixian:data" + encodeURIComponent(data); },
-    sendNetworkProtocolData: function (protocolId, data) { location.href = "ixian:protocolData" + protocolId + "=" + encodeURIComponent(data); },
-    getStorageData: function (key) { location.href = "ixian:getStorageData" + encodeURIComponent(key); },
-    setStorageData: function (key, value) { location.href = "ixian:setStorageData" + encodeURIComponent(key) + "=" + encodeURIComponent(value); },
-    spixiAction: function (actionData) { location.href = "ixian:action" + encodeURIComponent(actionData); },
-
-    // on* handlers should be overriden by the app
-    onInit: function (sessionId, userAddresses) { /*alert("Received init with sessionId: " + sessionId + " and userAddresses: " + userAddresses);*/ },
-    onStorageData: function (key, value) { /*alert("Received storage data: " + key + "=" + value);*/ },
-    onNetworkData: function (senderAddress, data) { /*alert("Received network data from " + senderAddress + ": " + data);*/ },
-    onNetworkProtocolData: function (senderAddress, protocolId, data) { /*alert("Received network app protocol data from " + senderAddress + " - " + protocolId + ": " + data);*/ },
-    onRequestAccept: function (data) { /*alert("Received request accept: " + data);*/ },
-    onRequestReject: function (data) { /*alert("Received request reject: " + data);*/ },
-    onAppEndSession: function (data) { /*alert("Received app end session: " + data);*/ },
 };
